9e0079ffa16c145c5aaaffe7aefea98333d1b51e
bebees <bebees032@gmail.com>
2018-04-10 16:47:07 +0900

    hue2

--- bebeemaps/settings.py
+++ bebeemaps/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'UTF-8'
+LANGUAGE_CODE = 'ko'
 
 TIME_ZONE = 'Asia/Seoul'
 
