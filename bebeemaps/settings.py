47427e6db010b1775e71b6d1dbc708afe4b598e1
bebees <bebees032@gmail.com>
2018-04-10 16:37:16 +0900

    modify setting

--- bebeemaps/settings.py
+++ bebeemaps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'h_!yt_@s!=%!-hgol)vnnw3d3&pa6z(gplzefe&4wd4-etgq#7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'UTF-8'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
